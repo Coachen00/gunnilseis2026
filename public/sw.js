c9b5ce57a0a72bb4a07de84d2cc22d4d562b175f
Joel Sjöqvist <leojsjoqvist@gmail.com>
2026-07-22 09:34:00 +0200

    Refresh cached production assets

--- public/sw.js
+++ public/sw.js
@@ -8,7 +8,7 @@
  *
  * Versionera cachenamnet när formatet ändras så aktiva SWs städas.
  */
-const CACHE_VERSION = "v3";
+const CACHE_VERSION = "v4";
 const STATIC_CACHE = `static-${CACHE_VERSION}`;
 const RUNTIME_CACHE = `runtime-${CACHE_VERSION}`;
 
